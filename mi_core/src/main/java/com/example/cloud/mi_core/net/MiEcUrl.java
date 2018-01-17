6ccbeacebebe128265c7b9a5a35507a1963640cd
cloud9527 <cloud09527@gmail.com>
2018-01-17 23:21:16 +0800

    分类详情列表完成

--- mi_core/src/main/java/com/example/cloud/mi_core/net/MiEcUrl.java
+++ mi_core/src/main/java/com/example/cloud/mi_core/net/MiEcUrl.java
@@ -8,4 +8,6 @@ public class MiEcUrl {
     public static final String URL_HOST = "http://ac-ztozcvko.clouddn.com/";
     public static final String INDEX_DATA = "76b3589babb66412623d.json";
     public static final String SORT_LIST_DATA = "588ada5bdd49e94d04b7.json";
+    public static final String SORT_CONTENT_DATA_1 = "cd36ad805deb8ecf1282.json";
+    public static final String SORT_CONTENT_DATA_2 = "004f29868dc4407f0c51.json";
 }
--- mi_ec/src/main/java/com/example/cloud/mi/ec/main/sort/content/ContentDelegate.java
+++ mi_ec/src/main/java/com/example/cloud/mi/ec/main/sort/content/ContentDelegate.java
@@ -2,10 +2,20 @@ package com.example.cloud.mi.ec.main.sort.content;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
 import com.example.cloud.mi.ec.R;
+import com.example.cloud.mi.ec.R2;
 import com.example.cloud.mi_core.delegates.LatteDelegate;
+import com.example.cloud.mi_core.net.MiEcUrl;
+import com.example.cloud.mi_core.net.RestClient;
+import com.example.cloud.mi_core.net.callback.ISuccess;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by cloud on 2018/1/15.
@@ -14,6 +24,10 @@ import com.example.cloud.mi_core.delegates.LatteDelegate;
 public class ContentDelegate extends LatteDelegate {
     private static final String ARG_CONTENT_ID = "CONTENT_ID";
     private int mId;
+    private List<SectionBean> mData = null;
+    @BindView(R2.id.rv_list_content)
+    RecyclerView mRecyclerView = null;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,8 +51,26 @@ public class ContentDelegate extends LatteDelegate {
         return R.layout.delegate_list_content;
     }
 
+    private void initData() {
+        RestClient.builder()
+                .url(MiEcUrl.SORT_CONTENT_DATA_1)
+                .success(new ISuccess() {
+                    @Override
+                    public void onSuccess(String response) {
+                        mData = new SectionDataConverter().conver(response);
+                        SectionAdapter sectionAdapter = new SectionAdapter(R.layout.item_section_content,
+                                R.layout.item_section_header, mData);
+                        mRecyclerView.setAdapter(sectionAdapter);
+                    }
+                })
+                .build().get();
+    }
+
+
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
+        final StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(manager);
+        initData();
     }
 }
