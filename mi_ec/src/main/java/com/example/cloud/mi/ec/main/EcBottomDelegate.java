f66e5739d3f30afef493cb97dcda09cb6381b1fe
cloud9527 <cloud09527@gmail.com>
2018-01-24 21:57:05 +0800

    整体UI完成，初稿做到这种程度，后续再优化

--- mi_ec/src/main/java/com/example/cloud/mi/ec/main/EcBottomDelegate.java
+++ mi_ec/src/main/java/com/example/cloud/mi/ec/main/EcBottomDelegate.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import com.example.cloud.mi.ec.main.cart.ShopCartDelegate;
 import com.example.cloud.mi.ec.main.discover.DiscoverDelegate;
 import com.example.cloud.mi.ec.main.index.IndexDelegate;
+import com.example.cloud.mi.ec.main.personal.PersonalDelegate;
 import com.example.cloud.mi.ec.main.sort.SortDelegate;
 import com.example.cloud.mi_core.delegates.bottom.BaseBottomDelegate;
 import com.example.cloud.mi_core.delegates.bottom.BottomItemDelegate;
@@ -25,7 +26,7 @@ public class EcBottomDelegate extends BaseBottomDelegate {
         items.put(new BottomTabBean("{fa-sort}", "分类"), new SortDelegate());
         items.put(new BottomTabBean("{fa-compass}", "发现"), new DiscoverDelegate());
         items.put(new BottomTabBean("{fa-shopping-cart}", "购物车"), new ShopCartDelegate());
-        items.put(new BottomTabBean("{fa-user}", "我的"), new IndexDelegate());
+        items.put(new BottomTabBean("{fa-user}", "我的"), new PersonalDelegate());
         return builder.addItems(items).build();
     }
 
